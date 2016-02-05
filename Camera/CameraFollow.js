6aa7633da36969d68cd5fcea28a4cc2d1147ac77
Merlup <merlinjackson88@gmail.com>
2016-02-04 23:01:53 -0800

    Updated Camera Switch on button input

--- Camera/CameraFollow.js
+++ Camera/CameraFollow.js
@@ -1,14 +1,17 @@
 ﻿#pragma strict
 
-public var distanceAway : float = 0;
-public var distanceUp : float = 0;
+//public var distanceAway : float = 0;
+//public var distanceUp : float = 0;
 public var smooth : float = 0;
 public var follow : Transform;
 private var targetPosition : Vector3 =  Vector3.zero;
 public var CurrentCam : float;
 public var Cameras : Vector2[];
-public var Camera1 : Vector2 = new Vector2(2, 2);
-public var Camera2 : Vector2 = new Vector2(4, 4);
+public var Camera1 : Vector2 = new Vector2(2, 0.7);
+public var Camera2 : Vector2 = new Vector2(2, 2);
+public var Camera3 : Vector2 = new Vector2(4, 4);
+public var Camera4 : Vector2 = new Vector2(6, 6);
+public var PlayerCam : Vector2;
 //public var Camera3 : Vector2 = new Vector2 ( distanceAway, distanceUp);
 //public var Camera4 : Vector2 = new Vector2 ( distanceAway, distanceUp);
 //public var Camera5 : Vector2 = new Vector2 ( distanceAway, distanceUp);
@@ -20,27 +23,31 @@ function Start () {
 	follow = GameObject.Find("CameraTarget").transform;
 	Cameras[0] = Camera1;
 	Cameras[1] = Camera2;
+	Cameras[2] = Camera3;
+	Cameras[3] = Camera4;
 	Debug.Log(Cameras[0]);
 	Debug.Log(Cameras[1]);
-	var distanceAway = Cameras[CurrentCam].x;
-	var distanceUp = Cameras[CurrentCam].y;
+	PlayerCam.x = Cameras[CurrentCam].x;
+	PlayerCam.y = Cameras[CurrentCam].y;
 	Debug.Log(Cameras);
 
 }
 
-function Update () {
-	if(Input.GetButton("CameraSwitch")) {
-
-
-	CurrentCam++;
-
-
-		if(CurrentCam >= 2) {
-			Debug.Log("Last Cam");
-			CurrentCam = 0; 
-		}
+function CameraSwitch () {
+	CurrentCam = CurrentCam + 1 * Time.deltaTime * 3;
+	if(CurrentCam >= 4) {
+	Debug.Log("Last Cam");
+	CurrentCam = 0; 
+	}
 
 	Debug.Log("Camera Switch");
+}
+
+function Update () {
+PlayerCam.x = Cameras[CurrentCam].x;
+	PlayerCam.y = Cameras[CurrentCam].y;
+	if(Input.GetButton("CameraSwitch")) {
+	CameraSwitch();
 	}
 //	if(Input.GetButton("Sprint")) {
 //	
@@ -65,9 +72,9 @@ function Update () {
 function LateUpdate () {
 
 
-	targetPosition = follow.position + follow.up * distanceUp - follow.forward * distanceAway;
-	Debug.DrawRay(follow.position, Vector3.up * distanceUp, Color.red);
-	Debug.DrawRay(follow.position, follow.forward * distanceUp, Color.red);
+	targetPosition = follow.position + follow.up * PlayerCam.y - follow.forward * PlayerCam.x;
+	Debug.DrawRay(follow.position, Vector3.up * PlayerCam.y, Color.red);
+	Debug.DrawRay(follow.position, follow.forward * PlayerCam.y, Color.red);
 	Debug.DrawLine(follow.position, targetPosition, Color.magenta);
 
 	transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
